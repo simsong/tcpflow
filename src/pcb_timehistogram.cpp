8836725b338c6990798a334e69f89ce1f98ee144
Michael Shick <mfshick@nps.edu>
2012-10-23 17:52:07 -0400

    Initial cairo rendering, however blank SVG produced

--- src/pcb_timehistogram.cpp
+++ src/pcb_timehistogram.cpp
@@ -10,12 +10,15 @@
  */
 
 #include "iface_pcb.h"
+#include <cairo/cairo.h>
+#include <cairo/cairo-svg.h>
 #include <vector>
 
 using std::vector;
 
 const int NUM_HISTOGRAMS = 6;
 const int NUM_BUCKETS = 3000;
+const int SVG_HEIGHT = 100.0;
 // to account for packets timestamped before the first we receive, start a
 // little ways into the buckets
 const int FIRST_BUCKET = NUM_BUCKETS / 10;
@@ -88,11 +91,13 @@ class histogram
 
             if(target_index < 0)
             {
+                std::cout << (int) span << " bucket underflow " << std::endl;
                 underflow_count++;
                 return;
             }
             if(target_index >= NUM_BUCKETS)
             {
+                std::cout << (int) span << " bucket overflow " << std::endl;
                 overflow_count++;
                 return;
             }
@@ -100,16 +105,134 @@ class histogram
             // FIXME take out debug
             std::cout << (int) span << " inserting into bucket " << target_index << std::endl;
 
-            bucket target_bucket = buckets[target_index];
+            bucket *target_bucket = &buckets.at(target_index);
 
             // FIXME
             // for now, assume everything is other until I figure out where to
             // grab port numbers from
-            target_bucket.other++;
+            (*target_bucket).other++;
         }
 };
 
+// this vector must hold histograms in order from greatest time resolution to
+// least
 vector<histogram> histograms;
+
+void render(const histogram selected_histogram)
+{
+    std::cout << "begin render!" << std::endl;
+    vector<bucket> buckets = selected_histogram.buckets;
+    // initial stat sweep:
+    //   - how many significant buckets are there
+    //     (between the first and last nonzero bucket)
+    //   - What is the tallest bucket?
+    int first_index = -1, last_index = -1, index = 0;
+    int num_sig_buckets = 0;
+    uint64_t greatest_bucket_sum = 0;
+    for(vector<bucket>::iterator bucket = buckets.begin();
+            bucket != buckets.end(); bucket++)
+    {
+        uint64_t bucket_sum = (*bucket).http + (*bucket).https
+            + (*bucket).other;
+
+        // look for first and last significant bucket
+        if(bucket_sum > 0)
+        {
+            last_index = index;
+            if(first_index < 0)
+            {
+                first_index = index;
+            }
+        }
+
+        // look for tallest bucket (most packets)
+        if(bucket_sum > greatest_bucket_sum)
+        {
+            bucket_sum = greatest_bucket_sum;
+        }
+
+        index++;
+    }
+    // if there's no first significant index, then there aren't any nonzero
+    // buckets.  Abort.
+    if(first_index < 0)
+    {
+        return;
+    }
+    num_sig_buckets = last_index - first_index;
+
+    cairo_t *cr;
+    cairo_surface_t *surface;
+ 
+    surface = (cairo_surface_t *) cairo_svg_surface_create("time_histogram.svg",
+            num_sig_buckets, SVG_HEIGHT);
+    cr = cairo_create(surface);
+
+    cairo_set_source_rgb(cr, 0, 0, 0);
+
+    std::cout << "rendering from " << first_index << " to " << last_index << std::endl;
+    // render pass
+    index = 0;
+    for(vector<bucket>::iterator bucket = buckets.begin() + first_index;
+            bucket != buckets.begin() + last_index; bucket++)
+    {
+        // TODO differentiate types
+        uint64_t bucket_sum = (*bucket).http + (*bucket).https
+            + (*bucket).other;
+        double bar_height = (((double) bucket_sum)
+                / ((double) greatest_bucket_sum)) * SVG_HEIGHT;
+
+        bar_height = (double) bucket_sum;
+
+        std::cout << "bar at " << index << "," << bar_height << std::endl;
+        cairo_rectangle(cr, index, bar_height, 1.0, bar_height);
+
+        index++;
+    }
+ 
+    //cairo_surface_flush(surface);
+    //cairo_surface_finish(surface);
+    cairo_destroy (cr);
+    cairo_surface_destroy(surface);
+}
+
+histogram select_for_render()
+{
+    histogram base = histograms.at(0);
+    uint64_t min_dropped = base.underflow_count + base.overflow_count;
+    histogram best = base;
+
+    // find the histogram with the lowest combined underflowed and overflowed
+    // packets
+    // the first histogram gets uselessly compared to itself for code simplicity
+    for(vector<histogram>::iterator candidate = histograms.begin();
+            candidate != histograms.end(); candidate++)
+    {
+        uint64_t dropped = (*candidate).underflow_count +
+            (*candidate).overflow_count;
+
+        // Since we want the most resolution and the histograms vector must be
+        // in descending resolution order, then if zero drops is ever
+        // encountered return that immediately since the following histograms
+        // with less resolution will also have zero drops but will be poorer
+        // representations
+        if(dropped == 0)
+        {
+            std::cout << "short-circuit selected " << (int) (*candidate).span << " as best histogram to render" << std::endl;
+            return *candidate;
+        }
+
+        if(dropped < min_dropped)
+        {
+            min_dropped = dropped;
+            best = *candidate;
+        }
+    }
+
+    std::cout << "selected " << (int) best.span << " as best histogram to render" << std::endl;
+
+    return best;
+}
 
 // The plugin callback itself
 void timehistogram(pcb::phase_t phase, const struct pcap_pkthdr *h,
@@ -118,18 +241,22 @@ void timehistogram(pcb::phase_t phase, const struct pcap_pkthdr *h,
     switch(phase)
     {
         case pcb::startup:
+            // create and insert histograms in descending time resolution order
             for(int ii = 0; ii < NUM_HISTOGRAMS; ii++)
             {
                 histograms.push_back(histogram((span_t)ii));
             }
             break;
         case pcb::scan:
-            for(int ii = 0; ii < NUM_HISTOGRAMS; ii++)
+            for(vector<histogram>::iterator histogram = histograms.begin();
+                    histogram != histograms.end(); histogram++)
             {
-                histograms[ii].ingest_packet(h, p);
+                (*histogram).ingest_packet(h, p);
             }
             break;
         case pcb::shutdown:
+            std::cout << "shutting down!" << std::endl;
+            render(select_for_render());
             break;
         case pcb::none:
         default:
