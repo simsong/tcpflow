bb511330c8af55789cb3dbc90b4d55ce3a2e8f9e
Michael Shick <mfshick@nps.edu>
2012-10-25 17:08:21 -0400

    Time histogram packet types now differentiated; started labeling

--- src/pcb_timehistogram.cpp
+++ src/pcb_timehistogram.cpp
@@ -9,28 +9,49 @@
  *
  */
 
-#include "iface_pcb.h"
 #include <cairo/cairo.h>
 #include <cairo/cairo-svg.h>
 #include <vector>
 
+#include "iface_pcb.h"
+
 using std::vector;
 
-const int NUM_HISTOGRAMS = 6;
-const int NUM_BUCKETS = 3000;
+#define NUM_HISTOGRAMS 6
+#define NUM_BUCKETS 3000
+//#define NUM_BUCKETS 100
 // SVG units are in pt
 // The size of the SVG ends up being (bar_width + bar_space) * bar_count
 // by height.  So really, these values serve to describe the aspect ratio
-const double SVG_HEIGHT = 40.0;
-const double SVG_BAR_WIDTH = 1.0;
-const double SVG_BAR_SPACE = 0.2;
-const double SVG_TITLE_FACTOR = 0.0455;
+#define GRAPH_HEIGHT 100.0
+#define GRAPH_WIDTH 161.803
+#define GRAPH_BAR_SPACE_FACTOR 1.2
+#define GRAPH_TITLE_FONT_BASE_SIZE 8.0
 // to account for packets timestamped before the first we receive, start a
 // little ways into the buckets
 const int FIRST_BUCKET = NUM_BUCKETS / 10;
 typedef enum {
     MINUTE = 0, HOUR, DAY, WEEK, MONTH, YEAR
 } span_t;
+
+#define IPV6_HEADER_LEN 40
+#define PORT_HTTP 80
+#define PORT_HTTPS 443
+
+// copied from tcpdemux.cpp - should this be in a header somewhere?
+struct private_ip6_hdr {
+	union {
+		struct ip6_hdrctl {
+			uint32_t ip6_un1_flow;	/* 20 bits of flow-ID */
+			uint16_t ip6_un1_plen;	/* payload length */
+			uint8_t  ip6_un1_nxt;	/* next header */
+			uint8_t  ip6_un1_hlim;	/* hop limit */
+		} ip6_un1;
+		uint8_t ip6_un2_vfc;	/* 4 bits version, top 4 bits class */
+	} ip6_ctlun;
+	struct private_in6_addr ip6_src;	/* source address */
+	struct private_in6_addr ip6_dst;	/* destination address */
+} __attribute__((__packed__));
 
 // Unit in libpcap is microsecond, so shall it be here
 const uint64_t span_lengths[] = {
@@ -52,6 +73,73 @@ struct bucket
 inline uint64_t extract_time(const struct pcap_pkthdr *h)
 {
     return (*h).ts.tv_usec + ((*h).ts.tv_sec * 1000000L);
+}
+
+// quickly try and get the port out of the packet.  It is assumed that the
+// packet is an IPv4 or 6 datagram in an ethernet frame.
+// any and all errors simply result in -1 being returned
+int try_and_get_port(const struct pcap_pkthdr *h, const u_char *p)
+{
+    // keep track of the length of the packet not yet examined
+    unsigned int unused_len = h->caplen;
+    if(unused_len < sizeof(struct ether_header))
+    {
+        return -1;
+    }
+    unused_len -= sizeof(struct ether_header);
+
+    struct ether_header *eth_header = (struct ether_header *) p;
+    u_short *ether_type_location = &eth_header->ether_type;
+    const u_char *ether_data = p + sizeof(struct ether_header);
+
+    /* Handle basic VLAN packets */
+    if(ntohs(*ether_type_location) == ETHERTYPE_VLAN)
+    {
+        // skip to real ethertype
+        ether_type_location += 2;
+        ether_data += 4;
+        unused_len -= 4;
+    }
+
+    // only one of these is safe to use!
+    const struct ip *ip_header = (struct ip *) ether_data;
+    const struct private_ip6_hdr *ip6_header
+        = (struct private_ip6_hdr *) ether_data;
+
+    u_char *ip_data;
+
+    switch(ntohs(*ether_type_location))
+    {
+        case ETHERTYPE_IP:
+            if(unused_len < sizeof(struct ip) ||
+                    ip_header->ip_p != IPPROTO_TCP)
+            {
+                return -1;
+            }
+            unused_len -= sizeof(struct ip);
+            ip_data = (u_char *) ether_data + sizeof(struct ip);
+            break;
+        case ETHERTYPE_IPV6:
+            if(unused_len < sizeof(struct private_ip6_hdr) ||
+                    ip6_header->ip6_ctlun.ip6_un1.ip6_un1_nxt != IPPROTO_TCP)
+            {
+                return -1;
+            }
+            unused_len -= sizeof(struct private_ip6_hdr);
+            ip_data = (u_char *) ether_data + sizeof(struct private_ip6_hdr);
+            break;
+        default:
+            return -1;
+    }
+
+    if(unused_len < sizeof(struct tcphdr))
+    {
+        return -1;
+    }
+
+    struct tcphdr *tcp_header = (struct tcphdr *) ip_data;
+
+    return ntohs(tcp_header->th_dport);
 }
 
 class histogram
@@ -97,26 +185,28 @@ class histogram
 
             if(target_index < 0)
             {
-                std::cout << (int) span << " bucket underflow " << std::endl;
                 underflow_count++;
                 return;
             }
             if(target_index >= NUM_BUCKETS)
             {
-                std::cout << (int) span << " bucket overflow " << std::endl;
                 overflow_count++;
                 return;
             }
 
-            // FIXME take out debug
-            std::cout << (int) span << " inserting into bucket " << target_index << std::endl;
-
             bucket *target_bucket = &buckets.at(target_index);
 
-            // FIXME
-            // for now, assume everything is other until I figure out where to
-            // grab port numbers from
-            (*target_bucket).other++;
+            switch(try_and_get_port(h, p))
+            {
+                case PORT_HTTP:
+                    target_bucket->http++;
+                    break;
+                case PORT_HTTPS:
+                    target_bucket->https++;
+                    break;
+                default:
+                    target_bucket->other++;
+            }
         }
 };
 
@@ -124,9 +214,9 @@ class histogram
 // least
 vector<histogram> histograms;
 
-void render(const histogram selected_histogram)
+void render(const histogram *selected_histogram)
 {
-    vector<bucket> buckets = selected_histogram.buckets;
+    vector<bucket> buckets = selected_histogram->buckets;
     // initial stat sweep:
     //   - how many significant buckets are there
     //     (between the first and last nonzero bucket)
@@ -169,29 +259,36 @@ void render(const histogram selected_histogram)
     cairo_t *cr;
     cairo_surface_t *surface;
     cairo_text_extents_t title_extents;
-    double offset_unit = SVG_BAR_WIDTH + SVG_BAR_SPACE;
 
+    double font_size_title = GRAPH_TITLE_FONT_BASE_SIZE;
+    // TODO remove
     const char *title = "Packets Received (One Minute)";
- 
+
+    //double output_width = num_sig_buckets * offset_unit;
+    //double output_height = GRAPH_HEIGHT + title_extents.height;
+
     surface = (cairo_surface_t *) cairo_svg_surface_create("time_histogram.svg",
-            num_sig_buckets * (offset_unit), SVG_HEIGHT);
+            GRAPH_WIDTH, GRAPH_HEIGHT);
     cr = cairo_create(surface);
 
+    // render title
     cairo_select_font_face (cr, "Monospace",
         CAIRO_FONT_SLANT_NORMAL, CAIRO_FONT_WEIGHT_NORMAL);
+    cairo_set_font_size (cr, font_size_title);
     cairo_set_source_rgb(cr, 0, 0, 0);
-    cairo_set_font_size (cr, num_sig_buckets * SVG_TITLE_FACTOR);
-
     cairo_text_extents(cr, title, &title_extents);
+    cairo_move_to(cr, (GRAPH_WIDTH - title_extents.width) / 2.0,
+            title_extents.height);
+    cairo_show_text(cr, title);
 
-    // destroy and rebuild surface with padding for text
-    //cairo_destroy (cr);
-    //cairo_surface_destroy(surface);
-    //surface = (cairo_surface_t *) cairo_svg_surface_create("time_histogram.svg",
-            //num_sig_buckets * (offset_unit), SVG_HEIGHT + title_extents.height);
-    //cr = cairo_create(surface);
+    // render bars
 
-    // render pass
+    // transform to render within the graph itself (inside title and labels)
+    cairo_translate(cr, 0, title_extents.height);
+    cairo_scale(cr, 1.0, 1.0 - (title_extents.height / GRAPH_HEIGHT));
+
+    double offset_unit = GRAPH_WIDTH / num_sig_buckets;
+    double bar_width = offset_unit / GRAPH_BAR_SPACE_FACTOR;
     index = 0;
     for(vector<bucket>::iterator bucket = buckets.begin() + first_index;
             bucket != buckets.begin() + last_index; bucket++)
@@ -200,17 +297,19 @@ void render(const histogram selected_histogram)
         uint64_t bucket_sum = (*bucket).http + (*bucket).https
             + (*bucket).other;
         double bar_height = (((double) bucket_sum)
-                / ((double) greatest_bucket_sum)) * SVG_HEIGHT;
+                / ((double) greatest_bucket_sum)) * GRAPH_HEIGHT;
 
         if(bar_height > 0)
         {
-            cairo_rectangle(cr, index * offset_unit, SVG_HEIGHT - bar_height,
-                    SVG_BAR_WIDTH, bar_height);
+            cairo_rectangle(cr, index * offset_unit, GRAPH_HEIGHT - bar_height,
+                    bar_width, bar_height);
             cairo_fill(cr);
         }
 
         index++;
     }
+    // reset translation
+    cairo_identity_matrix(cr);
  
     //cairo_surface_flush(surface);
     //cairo_surface_finish(surface);
@@ -218,40 +317,28 @@ void render(const histogram selected_histogram)
     cairo_surface_destroy(surface);
 }
 
-histogram select_for_render()
+histogram *select_for_render()
 {
-    histogram base = histograms.at(0);
-    uint64_t min_dropped = base.underflow_count + base.overflow_count;
-    histogram best = base;
+    // assume the lowest resolution histogram is best
+    histogram *best = &histograms.back();
 
-    // find the histogram with the lowest combined underflowed and overflowed
-    // packets
-    // the first histogram gets uselessly compared to itself for code simplicity
+    // use the highest resolution histogram with no overflowed packets, or use
+    // the lowest resolution histogram if all have overflow, since it should
+    // almost certainly have the least since it's span is the largest
+    // Histograms must be in descending order of resolution
     for(vector<histogram>::iterator candidate = histograms.begin();
             candidate != histograms.end(); candidate++)
     {
-        uint64_t dropped = (*candidate).underflow_count +
-            (*candidate).overflow_count;
+        uint64_t dropped = candidate->underflow_count +
+            candidate->overflow_count;
 
-        // Since we want the most resolution and the histograms vector must be
-        // in descending resolution order, then if zero drops is ever
-        // encountered return that immediately since the following histograms
-        // with less resolution will also have zero drops but will be poorer
-        // representations
         if(dropped == 0)
         {
-            std::cout << "short-circuit selected " << (int) (*candidate).span << " as best histogram to render" << std::endl;
-            return *candidate;
-        }
-
-        if(dropped < min_dropped)
-        {
-            min_dropped = dropped;
-            best = *candidate;
+            // this seems bad, but I don't think there's a better way.
+            best = &(*candidate);
+            break;
         }
     }
-
-    std::cout << "selected " << (int) best.span << " as best histogram to render" << std::endl;
 
     return best;
 }
@@ -277,7 +364,6 @@ void timehistogram(pcb::phase_t phase, const struct pcap_pkthdr *h,
             }
             break;
         case pcb::shutdown:
-            std::cout << "shutting down!" << std::endl;
             render(select_for_render());
             break;
         case pcb::none:
