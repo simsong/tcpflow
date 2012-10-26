ccdce1ac55c8fbaf3479d29c732982b9b4a0751c
Michael Shick <mfshick@nps.edu>
2012-10-26 17:54:24 -0400

    time_histogram is almost complete

--- src/pcb_timehistogram.cpp
+++ src/pcb_timehistogram.cpp
@@ -11,6 +11,7 @@
 
 #include <cairo/cairo.h>
 #include <cairo/cairo-svg.h>
+#include <iomanip>
 #include <vector>
 
 #include "iface_pcb.h"
@@ -27,6 +28,20 @@ using std::vector;
 #define GRAPH_WIDTH 161.803
 #define GRAPH_BAR_SPACE_FACTOR 1.2
 #define GRAPH_TITLE_FONT_BASE_SIZE 8.0
+#define GRAPH_TITLE_MAX_WIDTH_FACTOR 0.8
+#define GRAPH_TITLE_Y_PAD_FACTOR 2.0
+#define GRAPH_SUBTITLE_Y_PAD_FACTOR 0.2
+#define GRAPH_TITLE_TEXT "TCP Packets Received"
+#define GRAPH_SUBTITLE_FACTOR 0.4
+#define GRAPH_TICK_LENGTH 2.0
+#define GRAPH_TICK_WIDTH 0.2
+#define GRAPH_NUM_TICKS 5
+#define GRAPH_TICK_DUMMY "000"
+#define GRAPH_TICK_Y_PAD_FACTOR 1.5
+#define GRAPH_TICK_Y_FONT_BASE_SIZE 4.0
+#define GRAPH_BOTTOM_PAD 8.0
+#define GRAPH_RIGHT_PAD 24.0
+#define GRAPH_TICK_X_FONT_BASE_SIZE 4.0
 // to account for packets timestamped before the first we receive, start a
 // little ways into the buckets
 const int FIRST_BUCKET = NUM_BUCKETS / 10;
@@ -61,6 +76,23 @@ const uint64_t span_lengths[] = {
     /* week */ 7L * 24L * 60L * 60L * 1000L * 1000L,
     /* month */ 30L * 24L * 60L * 60L * 1000L * 1000L,
     /* year */ 12L * 30L * 24L * 60L * 60L * 1000L * 1000L
+};
+
+const char * units_strings[] = {
+    "packets vs time",
+    "kilopackets vs time",
+    "megapackets vs time",
+    "gigapackets vs time",
+    "terapackets vs time",
+    "petapackets vs time",
+    "exapackets vs time",
+};
+
+struct rgb
+{
+    double r;
+    double g;
+    double b;
 };
 
 struct bucket
@@ -204,6 +236,10 @@ class histogram
                 case PORT_HTTPS:
                     target_bucket->https++;
                     break;
+                case -1:
+                    // try_and_get_port() returns -1 for any error, including if
+                    // there isn't a TCP segment in the packet
+                    break;
                 default:
                     target_bucket->other++;
             }
@@ -214,8 +250,26 @@ class histogram
 // least
 vector<histogram> histograms;
 
+void time_format(struct tm *time, char *buf, int buflen)
+{
+    snprintf(buf, buflen, "%04d-%02d-%02d %02d:%02d:%02d", 1900 + time->tm_year,
+            1 + time->tm_mon, time->tm_mday, time->tm_hour, time->tm_min,
+            time->tm_sec);
+}
+
 void render(const histogram *selected_histogram)
 {
+    struct rgb color_http, color_https, color_other;
+    color_http.r = 0.05;
+    color_http.g = 0.33;
+    color_http.b = 0.65;
+    color_https.r = 0.00;
+    color_https.g = 0.75;
+    color_https.b = 0.20;
+    color_other.r = 1.00;
+    color_other.g = 0.77;
+    color_other.b = 0.00;
+
     vector<bucket> buckets = selected_histogram->buckets;
     // initial stat sweep:
     //   - how many significant buckets are there
@@ -256,36 +310,155 @@ void render(const histogram *selected_histogram)
     }
     num_sig_buckets = last_index - first_index;
 
+    //
+    // Start rendering
+    //
+
     cairo_t *cr;
     cairo_surface_t *surface;
-    cairo_text_extents_t title_extents;
-
-    double font_size_title = GRAPH_TITLE_FONT_BASE_SIZE;
-    // TODO remove
-    const char *title = "Packets Received (One Minute)";
-
-    //double output_width = num_sig_buckets * offset_unit;
-    //double output_height = GRAPH_HEIGHT + title_extents.height;
 
     surface = (cairo_surface_t *) cairo_svg_surface_create("time_histogram.svg",
             GRAPH_WIDTH, GRAPH_HEIGHT);
     cr = cairo_create(surface);
 
     // render title
-    cairo_select_font_face (cr, "Monospace",
+
+    cairo_text_extents_t title_extents;
+    cairo_text_extents_t subtitle_extents;
+    double font_size_title = GRAPH_TITLE_FONT_BASE_SIZE;
+
+    // choose subtitle based on magnitude of units
+    const char *subtitle = units_strings[0];
+    uint64_t unit_index = greatest_bucket_sum / 1000;
+    if(unit_index < (sizeof(units_strings) / sizeof(char *)))
+    {
+        subtitle = units_strings[greatest_bucket_sum / 1000];
+    }
+
+    cairo_select_font_face(cr, "Sans",
         CAIRO_FONT_SLANT_NORMAL, CAIRO_FONT_WEIGHT_NORMAL);
-    cairo_set_font_size (cr, font_size_title);
+    cairo_set_font_size(cr, font_size_title);
     cairo_set_source_rgb(cr, 0, 0, 0);
-    cairo_text_extents(cr, title, &title_extents);
+    cairo_text_extents(cr, GRAPH_TITLE_TEXT, &title_extents);
+    // Is the title too wide?
+    double title_max_width = GRAPH_WIDTH * GRAPH_TITLE_MAX_WIDTH_FACTOR;
+    if(title_extents.width > title_max_width)
+    {
+        // scale the font size accordingly
+        font_size_title *= title_max_width / title_extents.width;
+        cairo_text_extents(cr, GRAPH_TITLE_TEXT, &title_extents);
+    }
+    // derive subtitle size and measure
+    double font_size_subtitle = font_size_title * GRAPH_SUBTITLE_FACTOR;
+    cairo_set_font_size(cr, font_size_subtitle);
+    cairo_text_extents(cr, subtitle, &subtitle_extents);
+    double intertitle_padding = subtitle_extents.height *
+        GRAPH_SUBTITLE_Y_PAD_FACTOR;
+    cairo_set_font_size(cr, font_size_title);
+    double title_padded_height = title_extents.height *
+        GRAPH_TITLE_Y_PAD_FACTOR;
+    double titles_padded_height = title_padded_height + intertitle_padding +
+        subtitle_extents.height;
+    // render title text
     cairo_move_to(cr, (GRAPH_WIDTH - title_extents.width) / 2.0,
-            title_extents.height);
-    cairo_show_text(cr, title);
+            title_extents.height +
+            (title_padded_height - title_extents.height) / 2);
+    cairo_show_text(cr, GRAPH_TITLE_TEXT);
+    // render subtitle text
+    cairo_set_font_size(cr, font_size_subtitle);
+    cairo_move_to(cr, (GRAPH_WIDTH - subtitle_extents.width) / 2.0,
+            ((title_padded_height - title_extents.height) / 2) +
+            title_extents.height + intertitle_padding +
+            subtitle_extents.height);
+    cairo_show_text(cr, subtitle);
+
+    // render ticks
+
+    // y ticks (packet counts)
+
+    // scale raw bucket totals
+    double y_scale_range = greatest_bucket_sum;
+    if(unit_index > 0)
+    {
+        y_scale_range /= unit_index;
+    }
+
+    cairo_text_extents_t max_tick_extents;
+    cairo_set_font_size(cr, GRAPH_TICK_Y_FONT_BASE_SIZE);
+    cairo_text_extents(cr, GRAPH_TICK_DUMMY, &max_tick_extents);
+    double y_label_allotment = max_tick_extents.width * GRAPH_TICK_Y_PAD_FACTOR;
+    double left_padding = y_label_allotment + GRAPH_TICK_LENGTH;
+
+    // translate down so the top of the window aligns with the top of the graph
+    // itself
+    cairo_translate(cr, 0, titles_padded_height);
+    cairo_scale(cr, 1.0, 1.0 - (GRAPH_BOTTOM_PAD / GRAPH_HEIGHT));
+
+    double y_scale_interval = y_scale_range / GRAPH_NUM_TICKS;
+    double y_tick_spacing = GRAPH_HEIGHT / GRAPH_NUM_TICKS;
+    for(int ii = 0; ii < GRAPH_NUM_TICKS; ii++)
+    {
+        double yy = (ii * y_tick_spacing) - (GRAPH_TICK_WIDTH / 2);
+
+        char tick_label[255];
+        snprintf(tick_label, 255, "%d",
+                (int) ((GRAPH_NUM_TICKS - (ii + 1)) * y_scale_interval));
+        cairo_text_extents_t extents;
+        cairo_text_extents(cr, tick_label, &extents);
+        cairo_move_to(cr, (y_label_allotment - extents.width) / 2,
+                yy + (extents.height / 2));
+        cairo_show_text(cr, tick_label);
+
+        // tick mark (but not for 0)
+        if(ii < GRAPH_NUM_TICKS - 1)
+        {
+            cairo_rectangle(cr, y_label_allotment, yy, GRAPH_TICK_LENGTH,
+                    GRAPH_TICK_WIDTH);
+            cairo_fill(cr);
+        }
+    }
+    cairo_identity_matrix(cr);
+
+    // x ticks (time)
+
+    time_t start_unix = selected_histogram->base_time /
+        (1000 * 1000);
+    time_t stop_unix = (selected_histogram->base_time +
+            selected_histogram->length) / (1000 * 1000);
+    struct tm *start_time = localtime(&start_unix);
+    struct tm *stop_time = localtime(&stop_unix);
+    char start_str[255], stop_str[255];
+    time_format(start_time, start_str, 255);
+    time_format(stop_time, stop_str, 255);
+    cairo_text_extents_t start_extents, stop_extents;
+
+    cairo_set_font_size(cr, GRAPH_TICK_X_FONT_BASE_SIZE);
+    cairo_text_extents(cr, start_str, &start_extents);
+    cairo_text_extents(cr, stop_str, &stop_extents);
+
+    // translate
+    cairo_translate(cr, left_padding, 0);
+    cairo_scale(cr, 1.0 - ((left_padding + GRAPH_RIGHT_PAD) /
+                GRAPH_WIDTH), 1.0);
+
+    cairo_move_to(cr, 0, GRAPH_HEIGHT -
+            (GRAPH_BOTTOM_PAD - start_extents.height) / 2);
+    cairo_show_text(cr, start_str);
+    cairo_move_to(cr, GRAPH_WIDTH - stop_extents.width, GRAPH_HEIGHT -
+            (GRAPH_BOTTOM_PAD - stop_extents.height) / 2);
+    cairo_show_text(cr, stop_str);
+
+    cairo_identity_matrix(cr);
 
     // render bars
 
     // transform to render within the graph itself (inside title and labels)
-    cairo_translate(cr, 0, title_extents.height);
-    cairo_scale(cr, 1.0, 1.0 - (title_extents.height / GRAPH_HEIGHT));
+    cairo_translate(cr, left_padding,
+            titles_padded_height);
+    cairo_scale(cr,
+            1.0 - ((left_padding + GRAPH_RIGHT_PAD) / GRAPH_WIDTH),
+            1.0 -
+            ((titles_padded_height + GRAPH_BOTTOM_PAD) / GRAPH_HEIGHT));
 
     double offset_unit = GRAPH_WIDTH / num_sig_buckets;
     double bar_width = offset_unit / GRAPH_BAR_SPACE_FACTOR;
@@ -293,7 +466,6 @@ void render(const histogram *selected_histogram)
     for(vector<bucket>::iterator bucket = buckets.begin() + first_index;
             bucket != buckets.begin() + last_index; bucket++)
     {
-        // TODO differentiate types
         uint64_t bucket_sum = (*bucket).http + (*bucket).https
             + (*bucket).other;
         double bar_height = (((double) bucket_sum)
@@ -301,9 +473,42 @@ void render(const histogram *selected_histogram)
 
         if(bar_height > 0)
         {
-            cairo_rectangle(cr, index * offset_unit, GRAPH_HEIGHT - bar_height,
-                    bar_width, bar_height);
+            double http_height = (((double) bucket->http) /
+                    ((double) bucket_sum)) * bar_height;
+            double https_height = (((double) bucket->https) /
+                    ((double) bucket_sum)) * bar_height;
+            double other_height = (((double) bucket->other) /
+                    ((double) bucket_sum)) * bar_height;
+
+            double current_height = GRAPH_HEIGHT - bar_height;
+
+            // other (yellow)
+            cairo_set_source_rgb(cr, color_other.r, color_other.g,
+                    color_other.b);
+            cairo_rectangle(cr, index * offset_unit, current_height,
+                    bar_width, other_height);
             cairo_fill(cr);
+
+            current_height += other_height;
+
+            // HTTPS (green)
+            cairo_set_source_rgb(cr, color_https.r, color_https.g,
+                    color_https.b);
+            cairo_rectangle(cr, index * offset_unit, current_height,
+                    bar_width, https_height);
+            cairo_fill(cr);
+
+            current_height += https_height;
+
+            // HTTP (blue)
+            cairo_set_source_rgb(cr, color_http.r, color_http.g,
+                    color_http.b);
+            cairo_rectangle(cr, index * offset_unit, current_height,
+                    bar_width, http_height);
+            cairo_fill(cr);
+
+            // reset to black
+            cairo_set_source_rgb(cr, 0.0, 0.0, 0.0);
         }
 
         index++;
