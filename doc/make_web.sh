25da19c43600f388bfad773b540cebad635148f8
Simson Garfinkel <simsong@acm.org>
2013-03-08 15:00:57 -0500

    allows tcpdemux to be disabled

--- doc/make_web.sh
+++ doc/make_web.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 #
 # Create the files for the tcpflow website
-HTDOCS=/var/www/digitalcorpora/tcpflow/demo
+DEST=/var/www/digitalcorpora/tcpflow/demo
+TCPFLOW=../src/tcpflow
+
+if [ ! -d $DEST ]; then mkdir -s $DEST ; fi
+
+if [ ! -x $TCPFLOW ]; then (cd .. ; make ) ; fi
+
