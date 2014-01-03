388ef423e01f3756cf18df2a61ff1ce9292893a4
Simson Garfinkel <simsong@acm.org>
2014-01-03 10:22:40 -0500

    updated tests

--- tests/test-pdfs.sh
+++ tests/test-pdfs.sh
@@ -4,11 +4,11 @@
 . $srcdir/test-subs.sh
 
 # create PDFs for all of the pcap files
-for i in *.pcap
+for i in $DMPDIR/*.pcap
 do
   echo $i
   cmd "$TCPFLOW -Fg -e netviz -o tmp$$ -r $i"
   cmd "mv tmp$$/report.pdf `basename $i .pcap`.pdf"
   echo ""
-  /bin/rm -rf tmp$$
+  /bin/rm -rf tmp$$ test?.pdf
 done
