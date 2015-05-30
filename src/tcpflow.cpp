0f0bd22591deaab5bfd96786a0665d1896116dd7
apDataGuy <antonio.pena@tanium.com>
2015-05-30 01:33:57 -0400

    Added 'len' and 'caplen' figures per flow

--- src/tcpflow.cpp
+++ src/tcpflow.cpp
@@ -365,7 +365,7 @@ static void process_infile(const std::string &expression,const char *device,cons
 
     /* start listening or reading from the input file */
     if (infile == "") DEBUG(1) ("listening on %s", device);
-    if (pcap_loop(pd, -1, handler, (u_char *)tcpdemux::getInstance()) < 0){
+    if (pcap_loop(pd, -1, handler, (u_char *) tcpdemux::getInstance()) < 0){
 	
 	die("%s: %s", infile.c_str(),pcap_geterr(pd));
     }
