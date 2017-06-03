293d2580b2c53028b7b6ae651c8c276229542193
O. Libre <olibre@Lmap.org>
2017-06-03 17:45:59 +0200

    Improve pylint score

--- python/plugins/samplePlugin.py
+++ python/plugins/samplePlugin.py
@@ -1,48 +1,49 @@
-## This a demo of a simple plugin package which consists of a two functions.
+## Example of a python plugin for tcpflow.
+## This sample contains three functions.
 
-## The first function takes a string (application data from tcpflow's buffer) returns a sample message.
+## The first function takes a string and returns a sample message.
+## The input string contains the application data from tcpflow's buffer.
 
 def sampleFunction(appData):
-	return "This message appears in the report.xml file under the 'plugindata' tag."
+    return "This message appears in the 'plugindata' tag of report.xml."
 
-## The second function takes a string (application data from tcpflow's buffer) and writes the application (HTTP) header data to myOutput.txt in the python director. This function does not return and simply prints to stdout.
+## The second function takes a string (application data)
+## and writes the application (HTTP) header data to the file
+## myOutput.txt located in the python director.
+## This function does not return and simply prints to stdout.
 
 def headerWriter(appData):
-	fName = "myOutput.txt"
-	f = open("python/"+fName,'a')
-	headerFinish = appData.find("\r\n\r\n")+4
-	headerData = appData[:headerFinish+1]
-	f.write(headerData)
-	f.close()
-	print "Wrote data to "+fName
+    fName = "myOutput.txt"
+    f = open("python/" + fName, 'a')
+    headerFinish = appData.find("\r\n\r\n") + 4
+    headerData = appData[:headerFinish+1]
+    f.write(headerData)
+    f.close()
+    print "Wrote data to " + fName
 
-## The third function takes a string (application datafrom tcpflow's buffer) and returns the binary result of taking the HTTP message (without headers) and performing a bitwise xor operation with a sample key (defined inside the function).
+## The third function takes a string (application data)
+## parses the HTTP message (without headers)
+## performs a bitwise xor operation with a key defined in the function
+## and returns the text corresponding to this binary result.
 
 def xorOp(appData):
+    # Assume variable buffer includes message data.
+    dataStart = appData.find("\r\n\r\n") + 4
+    httpData = appData[dataStart:]
+    binaryData = ''.join(format(ord(x), 'b') for x in httpData)
+    if len(binaryData) < 1:
+        return 0
 
-	#assume variable buffer includes message data
-	
-	key = "01101011101"
-	newKey = ""
-	keyLen = len(key)
-
-	dataStart = appData.find("\r\n\r\n")+4
-	httpData = appData[dataStart:]
-	binaryData=''.join(format(ord(x), 'b') for x in httpData)
-	if len(binaryData)<1:
-		return 0
-	else:
-		while len(newKey) + keyLen <= len(binaryData):
-			newKey+=key
-
-		i=0	
-		while len(newKey) < len(binaryData):
-			if i == keyLen:
-				i = 0
-			newKey+=key[i]
-			i+=1
-
-		xorRes = int(binaryData,2) ^ int(newKey,2)
-
-		return '{0:b}'.format(xorRes)
-
+    key = "01101011101"
+    keyLen = len(key)
+    newKey = ""
+    while len(newKey) + keyLen <= len(binaryData):
+        newKey += key
+    i = 0
+    while len(newKey) < len(binaryData):
+        if i == keyLen:
+            i = 0
+        newKey += key[i]
+        i += 1
+    xorRes = int(binaryData,2) ^ int(newKey,2)
+    return '{0:b}'.format(xorRes)
