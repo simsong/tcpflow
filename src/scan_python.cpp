eab472ec40e972974026adfcb86c5460e0170f8b
O. Libre <olibre@Lmap.org>
2017-06-04 14:15:21 +0200

    Change scan_python parameters -S

--- src/scan_python.cpp
+++ src/scan_python.cpp
@@ -1,6 +1,5 @@
-/** emacs: -*- mode: C++; c-basic-offset: 4; indent-tabs-mode: nil; tab-width: 8; c-file-style: "linux" -*-
- *  vi:ft=cpp ts=8 et sw=4 sts=4 tw=80
- *
+/** vi:ft=cpp ts=8 et sw=4 sts=4 tw=80
+ *  emacs: -*- mode: C++; c-basic-offset: 4; indent-tabs-mode: nil; tab-width: 8; c-file-style: "linux" -*-
  *
  * scan_python:
  * Use external python script
@@ -21,19 +20,18 @@
 struct ScanPython
 {
     void startup(const scanner_params& sp);
-    void init(const scanner_params& sp);
-    void before(const scanner_params& sp);
+    void init();
+    void before();
     void scan(const scanner_params& sp);
-    void shutdown(const scanner_params& sp);
+    void shutdown();
 
-    std::string scriptFullPath;
-    std::string scriptDirectory;
-    std::string scriptFilename;
-    std::string functionName;
+    std::string path;
+    std::string module;
+    std::string function;
     std::string initializationScript;
 
 #if HAVE_PYTHON2_7_PYTHON_H
-    PyObject* pythonFunction = NULL; // requires C++11
+    PyObject* pythonFunction;
 #endif
 };
 
@@ -50,79 +48,85 @@ void ScanPython::startup(const scanner_params& sp)
     sp.info->name = "python";
     sp.info->flags = scanner_info::SCANNER_DISABLED;
 
-    sp.info->get_config("pyScript", &scriptFullPath, "pathname of a tcpdump-compatible python script");
-    sp.info->get_config("pyFunction", &functionName, "function name within the python script");
+    sp.info->get_config("pyPath", &path, "    Directory to find python module (optional)");
+    sp.info->get_config("pyModule", &module, "  Name of python module (script name without extension)");
+    sp.info->get_config("pyFunction", &function, "Function name within the python module");
+
+#if HAVE_PYTHON2_7_PYTHON_H
+    pythonFunction = NULL;
+#endif
 }
 
+static std::string get_working_dir(const std::string& path)
+{
+    if (path.empty()) {
+        return "os.getcwd()";
+    }
+    if (path[0] == '/'){
+        return "'" + path + "'";
+    } else {
+        return "os.getcwd() + '/" + path + "'";
+    }
+}
 
-void ScanPython::init(const scanner_params& /*sp*/)
+void ScanPython::init()
 {
 #ifndef HAVE_PYTHON2_7_PYTHON_H
     DEBUG(2)
-    ("tcpflow cannot call python scripts required by the scanner_python "
+    ("[scan_python] tcpflow cannot call python scripts required by the scanner 'python' "
      "because the header <python2.7/Python.h> was not present during the tcpflow build. "
      "Try to install package 'python-devel' and build again tcpflow (./configure)");
 #endif
 
-    if (scriptFullPath.empty() || functionName.empty()) {
-        DEBUG(2)("[scan_python] Cannot call python becase no script/function is provided."
-                 " Please use arguments -S pyScript=dir/script.py -S pyFunction=foo.");
+    if (module.empty() || function.empty()) {
+        DEBUG(2)("[scan_python] Cannot call python becase no provided module/function.\n"
+                 "              Please use arguments -S pyModule=module -S pyFunction=foo");
+        return;
     }
 
     // Write the initialization script to set directory to the local system path
-    initializationScript = "import sys, os"  "\n"
-                           "workingDir = ";
-    // Split directory/filename
-    size_t delimeter_index = scriptFullPath.rfind("/");
-    if (delimeter_index == std::string::npos) {
-        scriptDirectory = ".";
-        scriptFilename = scriptFullPath;
-        initializationScript += "os.getcwd()";
-    } else {
-        scriptDirectory = scriptFullPath.substr(0, delimeter_index);
-        scriptFilename = scriptFullPath.substr(delimeter_index + 1);
-        initializationScript += (scriptDirectory[0] == '/') ? "'" : "os.getcwd() + '/";
-        initializationScript += scriptDirectory + "'";
-    }
-    initializationScript += "\n" "sys.path.append(workingDir)" "\n";
+    initializationScript = "import sys, os"                          "\n"
+                           "workingDir = " + get_working_dir(path) + "\n"
+                           "sys.path.append(workingDir)"             "\n";
 
 #if HAVE_PYTHON2_7_PYTHON_H
     // Spawn python interpreter
     Py_Initialize();
 
-    DEBUG(10)
-    ("Initialize Python using script:" "\n" "%s",
-        initializationScript.c_str());
+    DEBUG(10) ("[scan_python]  Initialize Python using script:" "\n" "%s", initializationScript.c_str());
     PyRun_SimpleString(initializationScript.c_str());
 
     // Create PyObject from script filename
-    PyObject* pName = PyString_FromString(scriptFilename.c_str());
+    PyObject* pName = PyString_FromString(module.c_str());
     if (pName == NULL) {
-        DEBUG(2)
-        ("Cannot create PyObject from script filename '%s'", scriptFilename.c_str());
+        DEBUG(2) ("[scan_python] Cannot create PyObject from path='%s' and module='%s'"
+                  "Try using three arguments: -S pyPath=path -S pyModule=module -S pyFunction=foo",
+                  path.c_str(), module.c_str());
         return;
     }
 
     // Import script file in python interpreter
     PyObject* pModule = PyImport_Import(pName);
     if (pModule == NULL) {
-        DEBUG(2)
-        ("Cannot import script '%s' in Python interpreter", scriptFilename.c_str());
+        DEBUG(2) ("[scan_python] Cannot import module='%s' from path='%s' in Python interpreter"
+                  "Try using three arguments: -S pyPath=path -S pyModule=module -S pyFunction=foo",
+                  module.c_str(), path.c_str());
         return;
     }
 
     // Identify function to be used
-    pythonFunction = PyObject_GetAttrString(pModule, functionName.c_str());
+    pythonFunction = PyObject_GetAttrString(pModule, function.c_str());
     if (pythonFunction == NULL) {
-        DEBUG(2)
-        ("Cannot identify function '%s' in python script '%s'", functionName.c_str(), scriptFilename.c_str());
+        DEBUG(2) ("[scan_python] Cannot identify function='%s' in module='%s' from path='%s'"
+                  "Try using three arguments: -S pyPath=path -S pyModule=module -S pyFunction=foo",
+                  function.c_str(), module.c_str(), path.c_str());
         return;
     }
 #endif
 }
 
 
-void ScanPython::before(const scanner_params& /*sp*/)
+void ScanPython::before()
 {
 }
 
@@ -130,6 +134,12 @@ void ScanPython::before(const scanner_params& /*sp*/)
 void ScanPython::scan(const scanner_params& sp)
 {
 #if HAVE_PYTHON2_7_PYTHON_H
+    if (pythonFunction == NULL) {
+        init();
+        if (pythonFunction == NULL)
+            return;
+    }
+
     // Cast packet buffer contents into a string and create pyObject
     std::string data(reinterpret_cast<char const*>(sp.sbuf.buf));
     PyObject* pData = PyString_FromString(data.c_str());
@@ -143,20 +153,23 @@ void ScanPython::scan(const scanner_params& sp)
     if (pResult) {
         // If XML-reporting enabled => Insert the returned string from the function
         if (sp.sxml) {
-            (*sp.sxml) << "<plugindata>"
-                       << PyString_AsString(pResult)
-                       << "</plugindata>";
+            const char* ptr = PyString_AsString(pResult);
+            if (ptr)
+                (*sp.sxml) << "<scan_python_result>"<< ptr <<"</scan_python_result>";
+            else
+                (*sp.sxml) << "<scan_python_result/>";
         }
     }
 #endif
 }
 
 
-void ScanPython::shutdown(const scanner_params& /*sp*/)
+void ScanPython::shutdown()
 {
 #if HAVE_PYTHON2_7_PYTHON_H
     // Terminate the python interpreter and exit
     Py_Finalize();
+    pythonFunction = NULL;
 #endif
 }
 
@@ -171,30 +184,30 @@ extern "C" void scan_python(const scanner_params& sp,
     case scanner_params::PHASE_NONE:
         break;
 
-    // called in main thread to parse configuration
+    // Called in main thread to parse configuration
     // (also used to build the --help text)
     case scanner_params::PHASE_STARTUP:
         singleton.startup(sp);
         break;
 
-    // called in main thread after all scanners loaded
+    // Should be called in main thread after all scanners loaded (but never called)
     case scanner_params::PHASE_INIT:
-        singleton.init(sp);
+        singleton.init();
         break;
 
-    // called in worker thread before first scan
+    // Called in worker thread before first scan
     case scanner_params::PHASE_THREAD_BEFORE_SCAN:
-        singleton.before(sp);
+        singleton.before();
         break;
 
-    // called in worker thread for each sbuf
+    // Called in worker thread for each sbuf
     case scanner_params::PHASE_SCAN:
         singleton.scan(sp);
         break;
 
-    // called in main thread when scanner is shutdown
+    // Called in main thread when scanner is shutdown
     case scanner_params::PHASE_SHUTDOWN:
-        singleton.shutdown(sp);
+        singleton.shutdown();
         break;
     }
 }
