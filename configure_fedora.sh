190827dbc2cee6cbb791ca73ad23f4f526c48502
Simson L. Garfinkel <simsong@acm.org>
2017-07-11 21:52:19 -0400

    new script

---
+++ configure_fedora.sh
@@ -0,0 +1,9 @@
+sudo dnf install zlib
+sudo dnf install zlib-static
+sudo dnf install openssl-devel
+sudo dnf install boost-devel
+sudo dnf install cairo-devel libpcap-devel
+sudo dnl install emacs
+sudo dnf install emacs
+sudo dnf install libpcap-devel
+sudo dnf install cairo-devel
