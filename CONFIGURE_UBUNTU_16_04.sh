bf068fc080f7762d5943b79fd057e3c9515f1d6c
Simson Garfinkel <simsong@acm.org>
2017-07-14 19:30:41 -0700

    works on Ubuntu 16.04; moved version number to -dev

---
+++ CONFIGURE_UBUNTU_16_04.sh
@@ -0,0 +1,5 @@
+sudo apt-get install llvm-3.7 autoconf 
+sudo apt-get install -y emacs
+sudo apt-get install -y zlibc zlib1g-dev openssl libssl-dev libboost-dev libcairo2 libpcap-dev libcairo2-dev
+
+
